6f9dd7a2e6cbbd3db6aa1c5d3bd51280a5b1b020
Sylvain-MED <sylvain.galoustoff@gmail.com>
2023-12-09 15:24:41 +0100

    Modif tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,13 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 export const content = ["./*.{html,js}"];
 export const theme = {
-    extend: {
-        backgroundImage: {
-            'header': "url('./assets/img/banniere.jpg')",
-        }
-    },
-    container: {
-        center: true,
-    }
+    extend: {}
 };
 export const plugins = [];
