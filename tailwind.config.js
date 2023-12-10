37f5ff50f23b80dd60519dddbb859ff1d186dd57
Sylvain-MED <sylvain.galoustoff@gmail.com>
2023-12-10 14:11:28 +0100

    Static header done

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,12 @@
 /** @type {import('tailwindcss').Config} */
 export const content = ["./*.{html,js}"];
 export const theme = {
-    extend: {}
+  extend: {
+    colors: {
+      primary: "#FFD15B",
+      black: "#1B1B1B",
+      white: "#fff"
+    },
+  },
 };
 export const plugins = [];
